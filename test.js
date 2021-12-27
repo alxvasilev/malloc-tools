08532e53caef53b7cd9eb713c812bba71462beaf
Alexander Vassilev <av@mega.co.nz>
2021-12-27 17:28:03 +0200

    Updated test.js

--- test.js
+++ test.js
@@ -1,16 +1,30 @@
 var tools = require("./index.js");
+var xml2js;
+try {
+    xml2js = require("xml2js");
+} catch (ex)
+{
+}
+
 async function main() {
     tools.malloc_stats();
     console.log("=============TRIM==============");
     tools.malloc_trim(0);
     tools.malloc_stats();
 
-    let xml = tools.malloc_info();
-    console.log("info:", xml);
-    let json = await require("xml2js").parseStringPromise(xml);
-    console.log("json:", JSON.stringify(json));
-    console.log("arenas:", json.malloc.heap.length);
     let info = tools.mallinfo2();
     console.log("mallinfo:", JSON.stringify(info));
+
+    let xml = tools.malloc_info();
+    console.log("info:", xml);
+    if (xml2js) {
+        let json = await xml2js.parseStringPromise(xml);
+        console.log("json:", JSON.stringify(json));
+        console.log("arenas:", json.malloc.heap.length);
+    }
+    else
+    {
+        console.warn("To parse the malloc_info output, please install xml2js module");
+    }
 }
 main();
